21687042a77445861776cd88089d45b3c5c46241
Dario Velez Roman <dvelezroman@gmail.com>
2024-09-26 11:01:27 -0500

    second commit

---
+++ src/main/java/com/ivisystems/sistemainventario/SistemaInventario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ivisystems.sistemainventario;
+
+/**
+ *
+ * @author dvelezroman
+ */
+public class SistemaInventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
